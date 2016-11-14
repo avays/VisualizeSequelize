7dbaa1d84c332a28f106f717d9d6af9a85dad7d0
Anthony Ardito <anthony.ardito@gmail.com>
2016-11-14 11:01:14 -0500

    working on deploy

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
-// import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
-export default createStore(rootReducer, thunkMiddleware);
+export default createStore(rootReducer, applyMiddleware(thunkMiddleware));
